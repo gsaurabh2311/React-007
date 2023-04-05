6371e98b826aa6816aa82ff7c33acc92daa9ec06
Saurabh Gupta <saurabh.gupta@inmorphis.com>
2023-04-05 18:16:12 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", null , "Hello World");
+
+    const root = ReactDOM.createRoot(document.getElementById("root"))
+
+    console.log(heading)
+
+    root.render(heading);
